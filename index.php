983084f64f659607c1dcac368f3072b90f18126f
panntomime <cashd@osuosl.org>
2017-06-05 15:38:32 -0700

    Updated html to match

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
-            <div class="container-fluid">
+            <div class="container-fluid page-content inset">
                 <div class="row">
                     <div class="col-lg-12">
 						<h1>Climb On</h1>
