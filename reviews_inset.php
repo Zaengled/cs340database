a13d71bd1bffac92b5ee01d22192c65f72e06961
panntomime <cashd@osuosl.org>
2017-06-09 16:02:06 -0700

    Added review panel

--- reviews_inset.php
+++ reviews_inset.php
@@ -3,21 +3,20 @@
 echo "<a href='submitreview.php?objid=" .$_GET['objid']."&userName=Zaengru&type=1'><button type='button'>Submit Review</button></a>"
 ?>
 
-<table width="80%">
-    <tr>
-        <th>Stars</th>
-        <th>User</th>
-        <th>Review Date</th>
-    </tr>
+<div class="panel panel-default">
+    <div class="panel-header">
+        Reviews
+    </div>
     <?php
     if($reviews->num_rows > 0){
-        while($row = $reviews->fetch_assoc()) {
-            echo "<tr onclick='reviewModal(";
-            echo json_encode($row);
-            echo ")'><td>" . $row["stars"] . "</td><td>" . $row["userName"] . "</td><td class='timestamp'>" . $row["timestamp"] . "</td></tr>";
-        }
+    while($row = $reviews->fetch_assoc()) {
+    echo "<li onclick='reviewModal(";
+    echo json_encode($row);
+    echo ")'><span>" . $row["stars"] . "</span><span>" . $row["userName"] . "</span><span class='timestamp'>" . $row["timestamp"] . "</span></li>";
+    }
     } else {
-        echo "<tr><td>No reviews yet!</td></tr>";
+    echo "<tr><td>No reviews yet!</td></tr>";
     }
     ?>
-</table>
+
+</div>
