c08ada6386151f07c8c45331ad6be7f4eac83f46
panntomime <cashd@osuosl.org>
2017-06-09 14:43:24 -0700

    More fanciness

--- js/climbOn.js
+++ js/climbOn.js
@@ -1,7 +1,7 @@
 function Momentify(){
     var elements = document.getElementsByClassName("timestamp");
     for(var i=0; i < elements.length; i++){
-        elements[i].innerHTML = moment(elements[i].innerHTML).fromNow()
+        elements[i].innerHTML = moment(new Date(elements[i].innerHTML)).fromNow();
     }
 }
 
