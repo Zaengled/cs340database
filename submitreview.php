05cf34cb5fddc1ca2389cb347b26ba537413247c
zaengled <zaengled@oregonstate.edu>
2017-06-09 14:34:43 -0700

    fixing review stars submission

--- submitreview.php
+++ submitreview.php
@@ -18,28 +18,33 @@
 <div class="row">
     <div class="col-lg-12">
 		<h1>Climb On</h1>
-		<h2>Reviewing:
+		<h2>
 		<?php 
 			if($result->num_rows > 0){
 				$row = $result->fetch_assoc();
 				echo " " . $row["name"];
-				echo " As: " . $_GET["userName"] . "</h2>";
+				echo "</h2>";
 			}
 		?>
 		<form method="post">
+			<fieldset>
+			<legend>New Review:</legend>
 			<h4>Review Body:
 				<p>
 				<textarea name="description" rows="15" cols="60" required = "true" placeholder="Enter your description here, please use only 500 words."></textarea>
 				</p>
 			</h4>
-			<h4>Rating: 
-				<input type="radio" name="stars" value='1' checked> 1
-				<input type="radio" name="stars" value="2" > 2
-				<input type="radio" name="stars" value="3" > 3
-				<input type="radio" name="stars" value="4" > 4
-				<input type="radio" name="stars" value="5" > 5
+			<h4>Stars: 
+				<select name="stars">
+					<option value='1' > 1 </option>
+					<option value="2" > 2 </option>
+					<option value="3" > 3 </option>
+					<option value="4" > 4 </option>
+					<option value="5" > 5 </option>
+				</select>
 			</h4>
 			<button type="submit" name="submityes" value="Submit">Submit</button>
+			</fieldset>
 		</form>
     </div>
 	<?php 
@@ -47,10 +52,10 @@
 		$OBJID = $_GET['objid'];
 		$USERNAME = $_GET['userName'];
 		$CONTENT = $_POST['description'];
-		$STARS = $_POST['stars'];
+		$STARS = intval($_POST['stars']);
 		if($OBJID != NULL && $USERNAME != NULL && $CONTENT != NULL && $STARS != NULL){
 			$result2 = $mysqli->query("DELETE FROM `Reviews` WHERE `Reviews`.`objid` = '$OBJID' AND `Reviews`.`userName` = '$USERNAME'");
-			$result = $mysqli->query("INSERT INTO `Reviews` (`userName`, `objid`, `body`, `stars`) VALUES ('$USERNAME', '$OBJID', '$CONTENT', 'STARS')");
+			$result = $mysqli->query("INSERT INTO `Reviews` (`userName`, `objid`, `body`, `stars`) VALUES ('$USERNAME', '$OBJID', '$CONTENT', '$STARS')");
 		}
 	}
 	?>
