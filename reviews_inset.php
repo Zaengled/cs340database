64accf7cd6a0c679b1a8e954c80f470558717a20
panntomime <cashd@osuosl.org>
2017-06-13 16:56:14 -0700

    Changed link styling

--- reviews_inset.php
+++ reviews_inset.php
@@ -35,7 +35,7 @@
             echo "<span class=\"glyphicon glyphicon-plus-sign\"></span>";
             echo "</a>";
         } else {
-            echo '<a class="btn btn-link" role="button"'
+            echo '<a role="button"'
                 . "onclick=\"document.getElementById('id01').style.display='block'\">"
                 . "Login "
                 . "</a> to add a review!";
