d5c04c5fc3955b79f58206b11a433a89aa6214d6
panntomime <cashd@osuosl.org>
2017-06-13 16:21:05 -0700

    Adding rating to gym lists

--- gym_list.php
+++ gym_list.php
@@ -7,24 +7,30 @@ if (!$mysqli) {
 $sql = "SELECT * FROM `GymAndLocation`";
 $result = mysqli_query($mysqli, $sql);
 ?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3>Gym Locations</h3>
-    </div>
-    <div class="panel-body">
-        <ul class="list-group">
-            <?php
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<a class='list-group-item' href='gym_detail.php?objid=" .$row["objid"] ."'>".$row["name"]."</a>";
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3>Gym Locations</h3>
+        </div>
+        <div class="panel-body">
+            <ul class="list-group">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<a style='text-transform:capitalize;' class='list-group-item' href='gym_detail.php?objid="
+                            . $row["objid"] . "'>" . $row["name"]
+                            . '<small style="float:right;" class="rating">'
+                            . '<span class="glyphicon glyphicon-star"></span>'
+                            . '<?php echo $gym['rating']; ?>'
+                        . '</small>'
+                        . "</a>";
                 }
-            } else {
-                echo "<li>No results found</li>";
-            }
-            ?>
-        </ul>
+                } else {
+                    echo "<li>No results found</li>";
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-</div>
 
 
 <?php include 'footer.php' ?>
