2260ff1917c305ef558af3138100774dffc85d58
panntomime <cashd@osuosl.org>
2017-06-13 16:45:14 -0700

    Made the list actually work

--- gym_list.php
+++ gym_list.php
@@ -16,7 +16,7 @@ $result = mysqli_query($mysqli, $sql);
                 <?php
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo "<a style='text-transform:capitalize;' class='list-group-item' href='gym_detail.php?objid="
+                        echo "<a class='list-group-item' style='text-transform:capitalize;' class='list-group-item' href='gym_detail.php?objid="
                             . $row["objid"] . "'>" . $row["name"]
                             . '<small style="float:right;" class="rating">'
                             . '<span class="glyphicon glyphicon-star"></span> '
