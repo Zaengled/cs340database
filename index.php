fb63b49aefe5bd487cfc0f97e0d06169fc86e040
Zaengled <zaengled@oregonstate.edu>
2017-06-12 18:28:09 -0700

    Fixing index merge conflicts

--- index.php
+++ index.php
@@ -4,15 +4,6 @@ include 'connect.php';
 if (!$mysqli) {
     die('Could not connect: ' . mysqli_error());
 }
-<<<<<<< HEAD
-=======
-$sql1 = "SELECT * FROM `GymAndLocation` WHERE City = 'Corvallis' ";
-$sql2 = "SELECT * FROM `SiteAndLocation` WHERE City = 'Corvallis' ";
-$sql3 = "SELECT * FROM `StoreAndLocation` WHERE city = 'Corvallis' ";
-$result1 = mysqli_query($mysqli, $sql1);
-$result2 = mysqli_query($mysqli, $sql2);
-$result3 = mysqli_query($mysqli, $sql3);
->>>>>>> f7752c491947b5d9a829c598ae9e02a15a2c7f9f
 ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="js/locationgrab.js"></script>
@@ -26,17 +17,7 @@ $result3 = mysqli_query($mysqli, $sql3);
             </div>
             <ul class="list-group" id="gyms">
                 <?php
-<<<<<<< HEAD
                     echo "<li class='list-group-item'>Nothing Nearby!</li>";
-=======
-                    if ($result1->num_rows > 0) {
-                        while($row = $result1->fetch_assoc()) {
-                            echo "<a class='list-group-item' href='gym_detail.php?objid=".$row["objid"]."'>".$row["name"]."</a>";
-                        }
-                    } else {
-                        echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                    }
->>>>>>> f7752c491947b5d9a829c598ae9e02a15a2c7f9f
                 ?>
             </ul>
         </div>
@@ -46,37 +27,11 @@ $result3 = mysqli_query($mysqli, $sql3);
             </div>
             <ul class="list-group" id="stores">
                 <?php
-<<<<<<< HEAD
                     echo "<li class='list-group-item'>Nothing Nearby!</li>";
-=======
-                    if ($result3->num_rows > 0) {
-                        while($row = $result3->fetch_assoc()) {
-                            echo "<a class='list-group-item' href='#'>".$row["name"]."</a>";
-                        }
-                    } else {
-                        echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                    }
                 ?>
             </ul>
         </div>
-		<div class="panel panel-default">
-            <div class="panel-heading">
-                Outdoor Sites
-            </div>
-            <ul class="list-group">
-                <?php
-                    if ($result2->num_rows > 0) {
-                        while($row = $result2->fetch_assoc()) {
-                            echo "<a class='list-group-item' href='#'>".$row["name"]."</a>";
-                        }
-                    } else {
-                        echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                    }
->>>>>>> f7752c491947b5d9a829c598ae9e02a15a2c7f9f
-                ?>
-            </ul>
-        </div>
-		<div class="panel panel-default">
+	<div class="panel panel-default">
             <div class="panel-heading">
                 Outdoor Sites
             </div>
@@ -85,7 +40,7 @@ $result3 = mysqli_query($mysqli, $sql3);
                     echo "<li class='list-group-item'>Nothing Nearby!</li>";
                 ?>
             </ul>
-			<div style="display: none;" id="city"><span id="city"></span></div>
+	    <div style="display: none;" id="city"><span id="city"></span></div>
         </div>
     </div>
 </div>
