c25f0623533f0afa2511f65209201d94df323d81
panntomime <cashd@osuosl.org>
2017-06-14 13:41:51 -0700

    Updating lists

--- store_detail.php
+++ store_detail.php
@@ -16,32 +16,31 @@ if ($_GET["objid"]){
 
 <div class="row">
     <div class="col-lg-12">
-        <h1>Climb On</h1>
-        <h2> STORE:
-        <?php
-			echo " " . $location["name"] . "</h2>";
-			echo "About: " . $location["bio"] . "<br>";
-			echo "Address: " . $location["address"] . " " . $location["city"] . ", " . $location["state"] . " " . $location["zip"] . "<br>";
-			echo "Rating: " . $location["rating"];
-        ?>
-        <?php include 'reviews_inset.php'?>
-
-        <!-- The Modal -->
-        <div id="myModal" class="modal">
-            <!-- Modal content -->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close">&times;</span>
-                    <h2 id="mh1">Modal Header</h2>
-                </div>
-                <div class="modal-body">
-                    <p id="cont">Some text in the Modal Body</p>
-                </div>
-                <div class="modal-footer">
-                    <h3 id="mh2">Modal Footer</h3>
+        <div class="panel panel-default"> <!--Gym info panel-->
+            <div class="panel-heading">
+                <h2>
+                    <?php echo $location['name']; ?>
+                    <small style="float:right;" class="rating">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <?php echo $location['rating']; ?>
+                    </small>
+                </h2>
+            </div>
+            <div class="panel-body">
+                <p>
+                    <?php echo $location['bio']; ?>
+                </p>
+                <div class="well" style="margin-bottom:0;">
+                    <span class="glyphicon glyphicon-home"></span>
+                    <?php $address = "$location[address], $location[city], $location[state] $location[zip]";
+                    echo "<a target='_blank' href='https://maps.google.com/?q=$address'>";
+                    echo "$address "; ?>
+                    <span class="glyphicon glyphicon-new-window"></span>
+                    </a>
                 </div>
             </div>
         </div>
+        <?php include 'reviews_inset.php'?>
 
     </div>
 </div>
