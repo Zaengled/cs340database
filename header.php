cda381f961b6cd270442a02dee6a122f12e2b2ab
panntomime <cashd@osuosl.org>
2017-06-13 17:48:29 -0700

    Fixed jquery issues

--- header.php
+++ header.php
@@ -30,10 +30,10 @@
         function openModal(id, data) {
             var modal = $('#' + id);
             if (modal) {
-                modal.style.display = 'block';
+                modal.css('display','block');
                 var keys = Object.keys(data);
                 for (var i = 0; i < keys.length; i++) {
-                    $('#' + id + '_' + keys[i]).innerHTML = data[keys[i]];
+                    $('#' + id + '_' + keys[i]).html(data[keys[i]]);
                 }
             }
         }
