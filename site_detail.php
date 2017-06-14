0267f713a65b4aaa595b364ae659b39490304e25
panntomime <cashd@osuosl.org>
2017-06-14 13:14:21 -0700

    Updating site detail

--- site_detail.php
+++ site_detail.php
@@ -1,51 +1,47 @@
-<?php include 'header.php'?>
+<?php include 'header.php' ?>
 <?php
 include 'connect.php';
-if ($_GET["objid"]){
-    $sql = "SELECT * FROM `SiteAndLocation` WHERE `objid` = ".$_GET["objid"];
+if ($_GET["objid"]) {
+    $sql = "SELECT * FROM `SiteAndLocation` WHERE `objid` = " . $_GET["objid"];
     $result = mysqli_query($mysqli, $sql);
-    $sql2 = "SELECT * FROM `Reviews` WHERE `objid` = ".$_GET["objid"];
+    $sql2 = "SELECT * FROM `Reviews` WHERE `objid` = " . $_GET["objid"];
     $reviews = mysqli_query($mysqli, $sql2);
-    $sql3 = "SELECT * FROM `Route` WHERE `locationID` = ".$_GET["objid"];
+    $sql3 = "SELECT * FROM `Route` WHERE `locationID` = " . $_GET["objid"];
     $routes = mysqli_query($mysqli, $sql3);
-	
-	if ($result->num_rows > 0) {
+
+    if ($result->num_rows > 0) {
         $location = $result->fetch_assoc();
     }
 }
 ?>
 
-<div class="row">
-    <div class="col-lg-12">
-        <h1>Climb On</h1>
-        <h2> SITE:
-        <?php
-			echo " " . $location["name"] . "</h2>";
-			echo "About: " . $location["bio"] . "<br>";
-			echo "Address: " . $location["address"] . " " . $location["city"] . ", " . $location["state"] . " " . $location["zip"] . "<br>";
-			echo "Rating: " . $location["rating"];
-        ?>
-        <?php include 'reviews_inset.php'?>
-
-        <?php include 'routes_inset.php'?>
-
-        <!-- The Modal -->
-        <div id="myModal" class="modal">
-            <!-- Modal content -->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close">&times;</span>
-                    <h2 id="mh1">Modal Header</h2>
-                </div>
-                <div class="modal-body">
-                    <p id="cont">Some text in the Modal Body</p>
-                </div>
-                <div class="modal-footer">
-                    <h3 id="mh2">Modal Footer</h3>
-                </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <?php echo $location["name"]; ?>
+            <span class='stars' style="float:right;">
+            <?php
+            echo "$location[stars] ";
+            for ($i = 1; $i <= 5; $i++) {
+                if ($i <= $location['stars']) {
+                    echo "<span class='glyphicon glyphicon-star'></span>";
+                } else {
+                    echo "<span class='glyphicon glyphicon-star-empty'></span>";
+                }
+            } ?>
+        </span>
+        </div>
+        <div class="panel-body">
+            <?php echo $location['bio']; ?>
+            <div class="well">
+                <h4>Address</h4>
+                <?php echo "$location[address] $location[city], $location[state] $location[zip]"; ?>
             </div>
         </div>
-
     </div>
-</div>
+
+<?php include 'reviews_inset.php' ?>
+
+<?php include 'routes_inset.php' ?>
+
+
 <?php include 'footer.php' ?>
