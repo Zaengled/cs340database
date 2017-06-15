39bd842cba8b6e9d3860cf3b9b07a13266a3a792
Zaengled <zaengled@oregonstate.edu>
2017-06-15 16:37:39 -0700

    Cleaning up about.php for our final project turn in

--- about.php
+++ about.php
@@ -1,13 +1,30 @@
 <?php include 'header.php' ?>
 <div class="row">
     <div class="col-lg-12">
-        <h1>Climb On</h1>
         <h2>Site Guide</h2>
-        <p>To navigate, use the Menu button above. To view a list of Gyms, Outdoor locations, or stores click one of the links in the menu.</p>
+        <p>To navigate, use the Menu on the left. To view a list of Gyms, Outdoor locations, or stores click one of the links in the menu.</p>
         <p>To post new routes, suggest new Gyms, Stores, or Outdoor locations, and leave reviews you will need to have an account.</p>
-        <p>Click the Login button in the menu popup to create a new account and begin! </p>
-        <h2>FAQ</h2>
-        <h2>About Us</h2>
+        <p>Click the Login button in the top right to create a new account and begin! </p>
+        <table><legend>FAQ</legend>
+			<tr><td>Q:</td>
+				<td>Do I have to be logged in to use the site?</td>
+			</tr>
+			<tr><td>A:</td>
+				<td>You only need to log in if you want to post new routes, reviews, or outdoor sites, or if you want to suggest a new store or gym. You are able to view all of these without logging in.</td>
+			</tr>
+			<tr><td>Q:</td>
+				<td>Can I be an admin?</td>
+			</tr>
+			<tr><td>A:</td>
+				<td>No.</td>
+			</tr>
+			<tr><td>Q:</td>
+				<td>Why do my suggested outdoor sites show up but gyms and stores don't?</td>
+			</tr>
+			<tr><td>A:</td>
+				<td>User submitted sites appear automatically but gyms and stores don't because admins must aprove them to make sure they are accurate.</td>
+			</tr>
+		</table>
     </div>
 </div>
 <?php include 'footer.php' ?>
