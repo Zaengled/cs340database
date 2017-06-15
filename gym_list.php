ddef5c8247026efe4acead3526f499e01f576cef
panntomime <cashd@osuosl.org>
2017-06-15 15:14:39 -0700

    Working on new gym modal

--- gym_list.php
+++ gym_list.php
@@ -27,6 +27,7 @@ $result = mysqli_query($mysqli, $sql);
                     <button class="btn btn-primary" type="submit">Submit</button>
                 </form>
             </div>
+            <div class="modal-footer"></div>
         </div>
     </div>
 
