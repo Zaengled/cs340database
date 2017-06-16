95aec28e3ac57750a257346d07e7710c98188b94
panntomime <cashd@osuosl.org>
2017-06-16 11:47:42 -0700

    Unifying post upload

--- store_detail.php
+++ store_detail.php
@@ -19,27 +19,27 @@ if ($_GET["objid"]) {
 
 }
 ?>
-
-    <div class="panel panel-default"> <!--Gym info panel-->
-        <div class="panel-heading">
-            <h2>
-                <?php echo $location['name']; ?>
-                <small style="float:right;" class="rating">
-                    <span class="glyphicon glyphicon-star"></span>
-                    <?php echo $location['rating']; ?>
-                </small>
-            </h2>
-        </div>
-        <div class="panel-body">
-            <div class="col-md-3">
-                <?php echo "<img class='img-responsive' src='$location[image]'>";?>
+    <div class="row">
+        <div class="panel panel-default"> <!--Gym info panel-->
+            <div class="panel-heading">
+                <h2>
+                    <?php echo $location['name']; ?>
+                    <small style="float:right;" class="rating">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <?php echo $location['rating']; ?>
+                    </small>
+                </h2>
             </div>
-            <div class="col-md-9">
-                <p>
-                    <?php echo $location['bio'];?>
-                </p>
+            <div class="panel-body">
+                <div class="col-md-3">
+                    <?php echo "<img class='img-responsive' src='$location[image]'>"; ?>
+                </div>
+                <div class="col-md-9">
+                    <p>
+                        <?php echo $location['bio']; ?>
+                    </p>
+                </div>
             </div>
-        </div>
             <div class="row well" style="margin-bottom:0;">
                 <span class="glyphicon glyphicon-home"></span>
                 <?php $address = "$location[address], $location[city], $location[state] $location[zip]";
