f9e03204cbdc30e1d6a11b7d0ebab9cb4591dab6
panntomime <cashd@osuosl.org>
2017-06-15 21:07:23 -0700

    Added address to new gym form

--- gym_detail.php
+++ gym_detail.php
@@ -3,47 +3,31 @@
 <?php
 include 'connect.php';
 
-$location = array(
-        'name' => $_POST['title'],
-        'bio' => $_POST['gym_body'],
-        'address' => $_POST['street'],
-        'city' => $_POST['city'],
-        'state' => $_POST['state'],
-        'zip' => $_POST['zip']
-    );
-
 $mysqli->query("INSERT INTO Location (type, city, state, zip, address) "
                 ."VALUES ('1', '$_POST[city]', '$_POST[state]', '$_POST[zip]', '$_POST[street]')");
 $id = $mysqli->insert_id;
-echo "RETURNED OBJID:$id<br>";
 
-echo 'POST:<br>';
-foreach ($_POST as $key => $value) {
-    echo "Key: $key; Value: $value<br>";
-}
-echo '<br><br>';
-
-/*if (isset($_POST["fileToUpload"])) {
+if (isset($_POST["fileToUpload"])) {
 
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $target_file = $target_dir . htmlspecialchars($_POST['name']);
     $uploadOk = 1;
+    $error = null;
     $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 // Check if image file is a actual image or fake image
     if (isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            $error = "File is not an image.";
             $uploadOk = 0;
         }
     }
 // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        $error = "This file already exists.";
         $uploadOk = 0;
     }
 // Check file size
@@ -55,34 +39,25 @@ echo '<br><br>';
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif"
     ) {
-        //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $error = "Only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        //echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+        echo "<div class='panel panel-danger'><div class='panel-body'>$error</div></div>";
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
             $location['image'] = $target_file;
         } else {
-            //echo "Sorry, there was an error uploading your file.";
+            echo "<div class='panel panel-danger'><div class='panel-body'>There was an error uploading your file</div></div>";
         }
     }
-}*/
-
-if ($mysqli->query("INSERT INTO Gym (gymId, name, bio, published) VALUES ".
-        "('$id', '$_POST[title]','$_POST[gym_body]', '0')")) {
-    echo "SQL Query was successful";
-} else {
-    echo "SQL Query was unsuccessful";
 }
-echo "<br>";
 
-?>
+$mysqli->query("INSERT INTO Gym (gymId, name, bio, published) VALUES ".
+        "('$id', '$_POST[title]','$_POST[gym_body]', '0')");
 
-<?php
 if ($_GET["objid"] || $id) {
     if ($_GET["objid"]){$id=$_GET["objid"];}
 
