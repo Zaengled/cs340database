95c1ccfdbe28dc50f74d8216382353bb7f855c4b
panntomime <cashd@osuosl.org>
2017-06-15 21:55:03 -0700

    Working on admin controls

--- admin_list.php
+++ admin_list.php
@@ -3,6 +3,7 @@ include 'header.php';
 include 'connect.php';
 if ($_SESSION['admin']) {
     if (isset($_GET['approve'])){
+        echo $_GET['approve'];
         $mysqli->query("CALL publishGymOrStore($_GET[approve])");
     }
     if (isset($_GET['delete'])){
