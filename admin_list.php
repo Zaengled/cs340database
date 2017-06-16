43f4a569fdc5368ca45e89f1cc11e518cb267580
panntomime <cashd@osuosl.org>
2017-06-15 21:35:15 -0700

    Added address to new gym form

--- admin_list.php
+++ admin_list.php
@@ -1,7 +1,7 @@
 <?php
 include 'header.php';
 include 'connect.php';
-$gyms = mysqli_query($mysqli, 'SELECT * FROM Gym WHERE published = "0"');
+$gyms = mysqli_query($mysqli, 'SELECT * FROM GymAndLocation WHERE published = "0"');
 $stores = mysqli_query($mysqli, 'SELECT * FROM Store where published = "0"');
 
 if ($_SESSION['admin']) { ?>
