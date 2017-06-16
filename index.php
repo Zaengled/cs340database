c8781c0602859bc61d84b756197273b8078b4f61
panntomime <cashd@osuosl.org>
2017-06-15 18:46:49 -0700

    Changing home page search

--- index.php
+++ index.php
@@ -9,25 +9,23 @@ if (!$mysqli) {
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="js/locationgrab.js"></script>
 <div class="row">
-    <form style="margin-top:10px; margin-bottom:10px;" class="col-lg-6 col-lg-offset-3">
-        <div class="input-group">
+    <div class="col-lg-12">
+        <div class="col-md-6 col-md-offset-3 input-group">
             <span class="input-group-btn">
                 <button class="btn btn-default"><span class="glyphicon glyphicon-screenshot"></span></button>
             </span>
-            <input class="form-control" type="text" id="city" placeholder="Search for nearby locations!">
+            <input class="form-control" type="text" id="city" placeholder="Enter City...">
             <span class="input-group-btn">
                 <button class="btn btn-primary" onclick="grabResults()">Search</button>
             </span>
         </div>
-    </form>
-    <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-heading">
                 Gyms
             </div>
             <ul class="list-group" id="gyms">
                 <?php
-                echo "<li class='list-group-item'>Nothing Nearby!</li>";
+                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
                 ?>
             </ul>
         </div>
@@ -37,20 +35,20 @@ if (!$mysqli) {
             </div>
             <ul class="list-group" id="stores">
                 <?php
-                echo "<li class='list-group-item'>Nothing Nearby!</li>";
+                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
                 ?>
             </ul>
         </div>
-        <div class="panel panel-default">
+		<div class="panel panel-default">
             <div class="panel-heading">
                 Outdoor Sites
             </div>
             <ul class="list-group" id="outdoor">
                 <?php
-                echo "<li class='list-group-item'>Nothing Nearby!</li>";
+                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
                 ?>
             </ul>
-            <div style="display: none;" id="city"><span id="city"></span></div>
+	    <div style="display: none;" id="city"><span id="city"></span></div>
         </div>
     </div>
 </div>
