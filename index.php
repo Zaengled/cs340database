46bda9d70f329d34a8ab84b9139dce415fe37fb3
panntomime <cashd@osuosl.org>
2017-06-15 18:31:45 -0700

    Changing home page search

--- index.php
+++ index.php
@@ -11,47 +11,47 @@ if (!$mysqli) {
 <div class="row">
     <div class="col-lg-12">
         <form class="col-lg-6 col-lg-offset-3">
-        <div class="input-group">
+            <div class="input-group">
             <span class="input-group-btn">
                 <button class="btn btn-default"><span class="glyphicon glyphicon-screenshot"></span></button>
             </span>
-            <input class="form-control" type="text" id="city" placeholder="Search for nearby locations!">
-            <span class="input-group-btn">
+                <input class="form-control" type="text" id="city" placeholder="Search for nearby locations!">
+                <span class="input-group-btn">
                 <button class="btn btn-primary" onclick="grabResults()">Search</button>
             </span>
-        </div>
         </form>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Gyms
-            </div>
-            <ul class="list-group" id="gyms">
-                <?php
-                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                ?>
-            </ul>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Stores
-            </div>
-            <ul class="list-group" id="stores">
-                <?php
-                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                ?>
-            </ul>
-        </div>
-		<div class="panel panel-default">
-            <div class="panel-heading">
-                Outdoor Sites
-            </div>
-            <ul class="list-group" id="outdoor">
-                <?php
-                    echo "<li class='list-group-item'>Nothing Nearby!</li>";
-                ?>
-            </ul>
-	    <div style="display: none;" id="city"><span id="city"></span></div>
-        </div>
     </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Gyms
+        </div>
+        <ul class="list-group" id="gyms">
+            <?php
+            echo "<li class='list-group-item'>Nothing Nearby!</li>";
+            ?>
+        </ul>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Stores
+        </div>
+        <ul class="list-group" id="stores">
+            <?php
+            echo "<li class='list-group-item'>Nothing Nearby!</li>";
+            ?>
+        </ul>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Outdoor Sites
+        </div>
+        <ul class="list-group" id="outdoor">
+            <?php
+            echo "<li class='list-group-item'>Nothing Nearby!</li>";
+            ?>
+        </ul>
+        <div style="display: none;" id="city"><span id="city"></span></div>
+    </div>
+</div>
 </div>
 <?php include 'footer.php'; ?>
